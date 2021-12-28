97018a7fe91eae4bab707425734f1a218ceafc4d
Németh Erik <nemetherik20@gmail.com>
2021-12-28 15:31:09 +0100

    Half-complete table in weekly view, the hours are filled dynamically

--- frontend/js/weekly.js
+++ frontend/js/weekly.js
@@ -3,16 +3,23 @@ const weekdaysUl = document.querySelector("#weekdaysUl");
 const weekdaysLIs = document.querySelectorAll(".weekday");
 const weekStepBack = document.querySelector("#weekStepBack");
 const weekStepForward = document.querySelector("#weekStepForward");
+const mainEventTable = document.querySelector("#mainEventTable");
 const eventTableContainer = document.querySelector("#eventTableContainer");
+const weekdaysTableRow = document.querySelector("#weekdays");
+const dayTableRows = document.querySelectorAll(".day");
 
+const dayTableRowsArray = Array.from(dayTableRows);
 const weekdaysLIsArray = Array.from(weekdaysLIs);
 const weekdays = getWeekdays();
 const monthNames = getMonthNames();
 
+let currentDate = new Date();
+
 document.addEventListener("DOMContentLoaded", e => {
-    fillDaysOfWeek();
-    createEventTableContainer();
-    console.log(eventTableContainer);
+    completeMainEventTable();
+    fillWeekdays();
+    fillDays(currentDate);
+
     // weekStepBack.addEventListener("click", stepBackInWeeks);
     // weekStepForward.addEventListener("click", stepForwardInWeeks);
 })
@@ -21,7 +28,7 @@ document.addEventListener("DOMContentLoaded", e => {
 /**
  * Fill the days of week part (header) with the name of the days.
  */
- function fillDaysOfWeek() {
+ function fillWeekdays() {
     for(let i = 0; i < 7; i++) {
         let weekdayName = weekdays[i];
         weekdaysLIsArray[i].innerHTML = weekdayName;
@@ -29,8 +36,45 @@ document.addEventListener("DOMContentLoaded", e => {
 }
 
 
-function createEventTableContainer() {
-    // let mainTable = document.createElement
+/**
+ * Fill the table row which contains the days/dates of the selected week.
+ * @param {Date} Date 
+ */
+function fillDays(date) {
+
+    let counter = 0;
+    dayTableRowsArray.forEach(dayTR => {
+        counter += 1;
+        if(dayTR) {
+            dayTR.innerHTML = counter;
+        }
+    });
+
+}
+
+
+/**
+ * Complete the main event table with the rows
+ */
+function completeMainEventTable() {
+    for(let i = 0; i < 24; i++) {  
+        let tr = document.createElement("tr");
+        let hourString = i < 10 ? "0" + i : i; 
+        let trContentHTML = `
+            <td class="event-time">${hourString}:00</td>
+            <td class="event-component d1"></td>
+            <td class="event-component d2"></td>
+            <td class="event-component d3"></td>
+            <td class="event-component d4"></td>
+            <td class="event-component d5"></td>
+            <td class="event-component d6"></td>
+            <td class="event-component d7"></td>
+        `;
+
+        tr.innerHTML = trContentHTML;
+
+        mainEventTable.appendChild(tr);
+    }
 }
 
 
