70206e4da107012acb5551ebf778de79500442a2
Laszlo Karker <laszlokarker@gmail.com@users.noreply.github.com>
2021-12-29 01:43:31 +0100

    API endpoints

--- backend/main.py
+++ backend/main.py
@@ -3,18 +3,10 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel, EmailStr
 from typing import Optional
 
-from sqlalchemy import create_engine
-#asd
-class LoginCredentials(BaseModel):
-    # this has to be the same name (and type (?)) as it's in the ajax request's body
-    email: EmailStr
-    password: str
+from sqlalchemy.sql.expression import distinct
+from connect import conn
+from models import users, User, events, Event, notifications, Notification, habits, Habit
 
-class RegisterCredentials(BaseModel):
-    # this has to be the same name (and type (?)) as it's in the ajax request's body
-    email: EmailStr
-    password: str
-    confirm_password: str
 
 
 app = FastAPI()
@@ -34,6 +26,124 @@ app.add_middleware(
     allow_headers = ["*"],
 )
 
+
+#Show all the data of a given id's user
+@app.get("/api/users/{id}")
+async def read_user_data(id: int):
+    return conn.execute(users.select().where(users.c.id == id)).fetchall()
+
+#Update the password of a given id's user
+@app.put("/api/users/{id}")
+async def update_password(id: int, user: User):
+    conn.execute(users.update().values(
+        password = user.password
+    ).where(users.c.id == id))
+    return conn.execute(users.select()).fetchall()
+
+#Show all the events of a given id's user
+@app.get("/api/users/{id}/events")
+async def read_user_events(id: int):
+    return conn.execute(events.select().where(events.c.user_id == id)).fetchall()
+
+#Add a new event for a given id's user
+@app.post("/api/users/{id}/events")
+async def add_user_event(event: Event):
+    conn.execute(events.insert().values(
+        id = event.id,
+        user_id = event.user_id,
+        color_id = event.color_id,
+        title = event.title,
+        description = event.description,
+        start_time = event.start_time,
+        end_time = event.end_time,
+        created_at = event.created_at
+    ))
+    return conn.execute(events.select()).fetchall()
+
+#Show a given event of a given id's user
+@app.get("/api/users/{user_id}/events/{event_id}")
+async def read_particular_user_event(user_id: int, event_id: int):
+    return conn.execute(events.select().where(events.c.user_id == user_id).where(events.c.id == event_id)).fetchall()
+
+
+#Update a given event of a given id's user
+@app.put("/api/users/{user_id}/events/{event_id}")
+async def update_particular_user_event(user_id: int, event_id: int, event: Event):
+    conn.execute(events.update().values(
+        description = event.description
+    ).where(events.c.user_id == user_id).where(events.c.id == event_id))
+    return conn.execute(events.select().where(events.c.user_id == user_id).where(events.c.id == event_id)).fetchall()
+
+#Get a certain user's certain event's all notifications
+@app.get("/api/users/{user_id}/events/{event_id}/notifications")
+async def read_particular_user_event_notifications(user_id: int, event_id: int):
+    return conn.execute(notifications.select().distinct().where(notifications.c.event_id == event_id).join(events, events.c.user_id == user_id)).fetchall()
+    #return conn.execute(events.select().where(events.c.user_id == user_id).where(events.c.id == event_id).join(notifications, events.c.id == notifications.c.event_id)).fetchall()
+
+#Delete a certain user's certain event's all notifications
+@app.delete("/api/users/{user_id}/events/{event_id}/notifications")
+async def delete_particular_user_event_notifications(user_id: int, event_id: int):
+    conn.execute(notifications.delete().where(notifications.c.event_id == event_id))
+    return conn.execute(notifications.select().distinct().where(notifications.c.event_id == event_id).join(events, events.c.user_id == user_id)).fetchall()
+
+#Show all the habits of a given id's user
+@app.get("/api/users/{id}/habits")
+async def read_user_habits(id: int):
+    return conn.execute(habits.select().where(habits.c.user_id == id)).fetchall()
+
+#Add a new habit for a given id's user
+@app.post("/api/users/{id}/habits")
+async def add_user_habit(habit: Habit):
+    conn.execute(habits.insert().values(
+        user_id = habit.user_id,
+        color_id = habit.color_id,
+        title = habit.title,
+        description = habit.description,
+        day_of_the_week = habit.day_of_the_week
+    ))
+    return conn.execute(events.select()).fetchall()
+
+#Show a given habit of a given id's user
+@app.get("/api/users/{user_id}/habits/{habit_id}")
+async def read_particular_user_habit(user_id: int, habit_id: int):
+    return conn.execute(habits.select().where(habits.c.user_id == user_id).where(habits.c.id == habit_id)).fetchall()
+
+
+#Update a habit of a given id's user
+@app.put("/api/users/{user_id}/habits/{habit_id}")
+async def update_habit(user_id: int, habit_id: int, habit: Habit):
+    conn.execute(habits.update().values(
+        color_id = habit.color_id,
+        title = habit.title,
+        description = habit.description,
+        day_of_the_week = habit.day_of_the_week
+    ).where(habits.c.user_id == user_id).where(habits.c.id == habit_id))
+    return conn.execute(habits.select().where(habits.c.user_id == user_id).where(habits.c.id == habit_id)).fetchall()
+
+
+'''
+#Show all data from the users table
+@app.get("/")
+async def read_data():
+    return conn.execute(users.select()).fetchall()
+
+
+
+
+
+#asd
+class LoginCredentials(BaseModel):
+    # this has to be the same name (and type (?)) as it's in the ajax request's body
+    email: EmailStr
+    password: str
+
+class RegisterCredentials(BaseModel):
+    # this has to be the same name (and type (?)) as it's in the ajax request's body
+    email: EmailStr
+    password: str
+    confirm_password: str
+
+
 @app.get("/")
 async def root():
     return {
@@ -42,7 +152,7 @@ async def root():
 
 @app.post("/api/login")
 async def user_login(loginCredentials: LoginCredentials):
-    """ User login with the credentials given by AJAX request. """
+    #User login with the credentials given by AJAX request.
 
     print("login credentials:", loginCredentials)
     # check, validate, etc..
@@ -56,7 +166,7 @@ async def user_login(loginCredentials: LoginCredentials):
 
 @app.post("/api/register")
 async def user_register(registerCredentials: RegisterCredentials):
-    """ User register with the credentials given by AJAX request. """
+    # User register with the credentials given by AJAX request.
     print("register credentials:", registerCredentials)
     # check, validate, etc..
 
@@ -67,6 +177,12 @@ async def user_register(registerCredentials: RegisterCredentials):
         return "not okay (invalid email and/or password)"
 
 
+
+
+
+
 # @app.get("/items/{item_id}")
 # def read_item(item_id: int, q: Optional[str] = None):
 #     return {"item_id": item_id, "q": q}
+
+'''
--- backend/models.py
+++ backend/models.py
@@ -6,7 +6,90 @@ from sqlalchemy import Table, MetaData, Column, ForeignKey, Integer, String, Dat
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql.expression import null
 from sqlalchemy.sql.sqltypes import SmallInteger
+from connect import meta
+from pydantic import BaseModel
 
+users = Table(
+    'users',meta,
+    Column('id',Integer, primary_key=True, index=True),
+    Column('email',String(255), unique=True, index=True),
+    Column('password',String(255))
+)
+
+class User(BaseModel):
+    id: str
+    email: str
+    password: str
+
+
+events = Table(
+    'events',meta,
+    Column('id',Integer, primary_key=True, index=True),
+    Column('user_id', Integer, ForeignKey('users.id')),
+    Column('color_id', SmallInteger, ForeignKey('colors.id')),
+    Column('title',String(255)),
+    Column('description',Text(16383), nullable=True),
+    Column('start_time',DateTime),
+    Column('end_time',DateTime),
+    Column('created_at',DateTime, default=datetime.datetime.utcnow()),
+)
+
+class Event(BaseModel):
+    id: str
+    user_id: str
+    color_id: str
+    title: str
+    description: str
+    start_time: str
+    end_time: str
+    created_at: str
+
+
+notifications = Table(
+    'notifications',meta,
+    Column('id', Integer, primary_key=True, index=True),
+    Column('event_id',Integer, ForeignKey('events.id')),
+    Column('type_id', SmallInteger, ForeignKey('notification_types.id')),
+    Column('trigger_time',DateTime)
+)
+
+class Notification(BaseModel):
+    id: str
+    event_id: str
+    type_id: str
+    trigger_time: str
+
+notification_types = Table(
+    'notification_types',meta,
+    Column('id',SmallInteger, primary_key=True),
+    Column('name', String(100))
+)
+
+class Notification_type(BaseModel):
+    id: str
+    name: str
+
+habits = Table(
+    'habits',meta,
+    Column('id',Integer, primary_key=True, index=True),
+    Column('user_id', Integer, ForeignKey('users.id')),
+    Column('color_id', SmallInteger, ForeignKey('colors.id')),
+    Column('title', String(255), nullable=True),
+    Column('description',Text(16383)),
+    Column('day_of_the_week', SmallInteger)
+)
+
+class Habit(BaseModel):
+    id: str
+    user_id : str
+    color_id : str
+    title : str
+    description: str
+    day_of_the_week: str
+
+
+
+'''
 Base = declarative_base()
 class User(Base):
     __tablename__ = "users"
@@ -54,3 +137,5 @@ class Notification(Base):
     event_id = Column(Integer, ForeignKey('events.id'))
     type_id = Column(SmallInteger, ForeignKey('notification_types.id'))
     trigger_time = Column(DateTime)
+
+'''
